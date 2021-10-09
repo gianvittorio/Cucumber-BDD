7ef832ff1ce8d85eae64da1d657130dbccd6a48d
gianvittorio <gianvittorio.castellano@gmail.com>
2021-10-09 19:19:36 -0300

    getting started with cucumber

---
+++ hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -0,0 +1,23 @@
+package hellocucumber;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinitions {
+
+    @Given("I am on Ebay Home page")
+    public void i_am_on_ebay_home_page() {
+        System.out.println("I am on Ebay Home page");
+    }
+
+    @When("I click on Advanced Link")
+    public void i_click_on_advanced_link() {
+        System.out.println("I click on Advanced Link");
+    }
+
+    @Then("I navigate to Advanced Search Page")
+    public void i_navigate_to_advanced_search_page() {
+        System.out.println("I navigate to Advanced Search Page");
+    }
+}
---
+++ hellocucumber/src/test/resources/hellocucumber/ebay_home.feature
@@ -0,0 +1,6 @@
+Feature: Ebay Home Page Scenarios
+
+  Scenario: Advanced Search Link
+    Given I am on Ebay Home page
+    When I click on Advanced Link
+    Then I navigate to Advanced Search Page
